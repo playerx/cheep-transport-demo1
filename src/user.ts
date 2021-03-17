6d7b1af8c6f31c134c30f581d4214ba2388fe9ce
playerx <ez@jok.io>
2021-03-17 15:38:27 +0400

    feat: implement initial user service

---
+++ src/user.ts
@@ -0,0 +1,29 @@
+import { MemoryTransport } from '@cheep/transport'
+
+async function run() {
+  const transport = new MemoryTransport()
+
+  await transport.init()
+
+  transport.on('User.login', ({ payload }) => {
+    const { username, password } = payload as any
+
+    return username === password
+  })
+
+  await transport.start()
+
+  const result = await transport.execute({
+    route: 'User.login',
+    payload: {
+      username: 'Me',
+      password: 'Me',
+    },
+  })
+
+  console.log('result', result)
+
+  await transport.dispose()
+}
+
+run()
